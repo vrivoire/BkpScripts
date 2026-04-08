6c260c4ce8e55da2d315a3852dde5adbac329d63
vrivoire <rivoire.vincent@gmail.com>
2026-04-08 06:18:00 -0400

    new files

---
+++ ThermoPro/Secrets.py
@@ -0,0 +1,7 @@
+OPEN_WEATHER_API_KEY = '10a108db2e0a8313f2487fb920090b1e'
+
+NEVIWEB_EMAIL="rivoire.vincent@gmail.com"
+NEVIWEB_PASSWORD="Mlvelc123."
+
+HYDRO_EMAIL="rivoire.vincent@gmail.com"
+HYDRO_PASSWORD="Elcv753."
