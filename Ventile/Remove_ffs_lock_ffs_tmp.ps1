6113b06ed59022cd06f1c2c18e8828511f52143c
vrivoire <rivoire.vincent@gmail.com>
2026-05-19 16:26:51 -0400

    added *.ffs_db

--- Ventile/Remove_ffs_lock_ffs_tmp.ps1
+++ Ventile/Remove_ffs_lock_ffs_tmp.ps1
@@ -8,6 +8,7 @@ Get-PSDrive -PSProvider FileSystem | ForEach-Object {
 		Get-ChildItem -Path "$env:HOMEDRIVE$env:HOMEPATH\" -Filter "RecycleBin*.ffs_tmp" -Directory -Recurse | Remove-Item -Recurse -Force
 	} else {		
 		Get-ChildItem -Path "$($_.Name):\" -Filter *.ffs_tmp -Recurse | Remove-Item -Force SilentlyContinue
-		Get-ChildItem -Path "$($_.Name):\" -Filter "RecycleBin*.ffs_tmp" -Directory -Recurse | Remove-Item -Recurse -Force 
+		Get-ChildItem -Path "$($_.Name):\" -Filter "*.ffs_tmp" -Directory -Recurse | Remove-Item -Recurse -Force 
+		Get-ChildItem -Path "$($_.Name):\" -Filter "*.ffs_db" -Directory -Recurse | Remove-Item -Recurse -Force 
 	}
 }
